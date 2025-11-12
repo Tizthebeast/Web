9634c391e5a3936982169e707ccf946213cc09cb
Tizthebeast <164633466+Tizthebeast@users.noreply.github.com>
2025-11-12 07:51:32 -0800

    nul

--- assets/js/math.js
+++ assets/js/math.js
@@ -1,11 +1,12 @@
 function add(){
-    let number1 = document.getElementById('input_1').value;
-    let number2 = document.getElementById('input_2').value;
+    let number1 = parseInt(document.getElementById('input_1').value);
+    let number2 = parseInt(document.getElementById('input_2').value);
+
     document.getElementById('decoy').innerHTML= number1+number2;
 }
 
 function Multiply(){
-    let number1 = document.getElementById('input_1').value;
-    let number2 = document.getElementById('input_2').value;
+    let number1 = parseInt(document.getElementById('input_1').value);
+    let number2 = parseInt(document.getElementById('input_2').value);
     document.getElementById('decoy').innerHTML= number1*number2;
 }
