241fdfcd8d5ef58ce69c2ee8dc928e29f2c94cb7
Tizthebeast <164633466+Tizthebeast@users.noreply.github.com>
2025-11-12 11:33:25 -0800

    nul

--- Server.js
+++ Server.js
@@ -14,14 +14,21 @@ connector.connect(function(err){
 });
 
 function SignUP(){
-    let Username = document.getElementById("Username");
-    let Password = document.getElementById("Password");
+    let UsernameDecoy = document.getElementById("Username");
+    let PasswordDecoy = document.getElementById("Password");
     connector.query(
         "SELECT * FROM authentication.info",(err,results) => {
             if(err) throw err;
             console.log(results);
         }); 
     connector.query(
-        "INSERT INTO info (Username,Password) values(?,?)",[]
-    )
+        "INSERT INTO info (Username,Password) values(?,?)",[UsernameDecoy,PasswordDecoy],
+        (err,result) => {
+            if(err){
+                console.error(err);
+                return res.status(500).send("Error inserting user");
+            }
+            res.send("User signed up successfully!");
+        }
+    )}
     
